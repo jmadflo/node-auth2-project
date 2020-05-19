b45e065e74ca8789b9eb7cb1100d1c0acc45375e
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-05-19 15:37:12 -0700

    Stretch goal: users can only see list of users from same department

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -7,7 +7,10 @@ const restricted = require('../auth/restrictedMiddleware.js')
 router.use(restricted)
 
 router.get('/', (req, res) => {
-    Users.find()
+    console.log(req.jwt)
+    
+    // Stretch: we only want users to be able to see users from the same department, so we filter the list of users accordingly
+    Users.findBy({ department: req.jwt.department })
         .then(users => {
             res.status(200).json({ users, jwt: req.jwt })
         })
